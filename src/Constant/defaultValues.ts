c4091c7790dd8afe78a3b920254a682be821d181
Nagarjunl <naga.career.at@gmail.com>
2025-10-23 12:24:37 +0530

    Fixed issue default params issue

--- src/Constant/defaultValues.ts
+++ src/Constant/defaultValues.ts
@@ -1,5 +1,10 @@
 import type { PaginationOptionsDto } from "../Dto/paginationOptionsDto";
 
+export const defaultparams = {
+  PageNumber: 1,
+  PageSize: 10,
+};
+
 export const DEFAULT_PAGINATION_OPTIONS: PaginationOptionsDto = {
   OrderBy: "createdAt",
   PageNumber: 1,
