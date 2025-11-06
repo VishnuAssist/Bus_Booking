d4449a156ccd8e2d76f49e591f2b10eac657fa37
Riyas123-K <157884467+Riyas123-K@users.noreply.github.com>
2025-11-06 22:25:05 +0530

    tooltip added

--- src/Layout/AppLayout/Header/index.tsx
+++ src/Layout/AppLayout/Header/index.tsx
@@ -1,73 +1,102 @@
 import {
   Box,
-  alpha,
   styled,
+  alpha,
   useTheme,
   Container,
   useMediaQuery,
   IconButton,
+  InputBase,
+  Paper,
+  Tooltip,
 } from "@mui/material";
 import Modechanger from "./ModeChanger";
 import SignOutButton from "./SignOut";
 import Logo from "../../../Assests/Icons/valiram2.png";
+
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 import SearchIcon from "@mui/icons-material/Search";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import AccessibilityNewSharpIcon from "@mui/icons-material/AccessibilityNewSharp";
+
+const HeaderWrapper = styled(Box)(({ theme }) => ({
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  zIndex: 10,
+  backgroundColor: alpha(
+    theme.header?.background || theme.palette.background.paper,
+    0.95
+  ),
+  backdropFilter: "blur(6px)",
+  boxShadow: "0 2px 8px rgba(0,0,0,0.08)",
+}));
 
 function Header() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const HeaderWrapper = styled(Box)(
-    ({ theme }) => `
-      right: 0;
-      left: 0;
-      z-index: 6;
-      background-color: ${alpha(
-        theme.header.background || theme.colors.alpha.black[100],
-        0.95
-      )};
-      backdrop-filter: blur(3px);
-      position: fixed;
-      width: 100%;
-      justify-content: space-between;
-  `
-  );
 
   return (
-    <HeaderWrapper sx={{ height: isMobile ? "60px" : theme.header.height }}>
+    <HeaderWrapper sx={{ height: isMobile ? 60 : theme.header.height }}>
       <Container
         maxWidth="xl"
         disableGutters
         sx={{
+          height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "space-between",
-          position: "relative",
-          minHeight: theme.header.height,
           px: 2,
+          position: "relative",
         }}
       >
+        <Box display="flex" alignItems="center" gap={1}>
+          {!isMobile && (
+            <Paper
+              sx={{
+                px: 1.5,
+                py: 0.2,
+                display: "flex",
+                alignItems: "center",
+                borderRadius: 3,
+              }}
+            >
+              <SearchIcon fontSize="small" />
+              <InputBase placeholder="Search..." sx={{ ml: 1, fontSize: 14 }} />
+            </Paper>
+          )}
+        </Box>
+
         {!isMobile && (
           <Box
             sx={{
               position: "absolute",
               left: "50%",
               transform: "translateX(-50%)",
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
             }}
           >
-            <img src={Logo} alt="Valiram" style={{ height: 40 }} />
+            <img src={Logo} alt="Logo" style={{ height: 40 }} />
           </Box>
         )}
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-        <Box display="flex" alignItems="center" sx={{ ml: "auto" }}>
-          <IconButton>
-            <NotificationsActiveIcon />
-          </IconButton>
+
+        <Box display="flex" alignItems="center" gap={1} ml="auto">
+          <Tooltip title="Notification">
+            <IconButton>
+              <NotificationsActiveIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Accessibility">
+            <IconButton>
+              <AccessibilityNewSharpIcon />
+            </IconButton>
+          </Tooltip>
           <Modechanger />
+          <Tooltip title="Profile">
+            <IconButton>
+              <AssignmentIndIcon />
+            </IconButton>
+          </Tooltip>
           <SignOutButton />
         </Box>
       </Container>
